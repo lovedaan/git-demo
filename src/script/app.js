ec3f5a6d4a5d0accb0fd2443136faf05ddda7311
赖海涛 <897955713@qq.com>
2016-10-15 20:25:48 +0800

    适合单页应用开发的gulp配置

--- src/script/app.js
+++ src/script/app.js
@@ -1,20 +1,24 @@
-!(function(){
-    var oBtn = document.querySelector('#btn');
+define('./app.js',[],function (require,exports,module) {
+    function limit(l,t,obj){
 
-    oBtn.onclick = function() {
-        var oBox = document.createElement('div');
-        oBox.style.width = '300px';
-        oBox.style.height = '300px';
-        oBox.className = 'box';
-        oBox.style.border = '1px solid blue';
-        document.body.appendChild(oBox);
-    };
-
-    document.onclick = function(ev){
-        var ev = ev || window.event;
-        if(ev.target.className == 'box'){
-            alert('这是新增加的盒子');
+        if(l <0){
+            l = 0;
         }
-    };   
-    
-})();
+        else if(l > document.documentElement.clientWidth - obj.offsetWidth){
+            l = document.documentElement.clientWidth - obj.offsetWidth;
+        }
+
+        if(t < 0){
+            t = 0;
+        }
+        else if(t > document.documentElement.clientHeight - obj.offsetHeight){
+            t = document.documentElement.clientHeight - obj.offsetHeight;
+        }
+
+
+        obj.style.left = l +'px';
+        obj.style.top = t +'px';
+    }
+
+    exports.limit = limit;
+});
--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,33 @@
-!(function(){
-    function Person(name,age){
-        this.name = name;
-        this.age = age;
+define('./index.js',['./app.js'],function (require,exports,module) {
+    var limit = require("./app.js");
+    function Drag () {
+        var oBox = document.getElementById('box');
+        var disX = 0, disY = 0;
+        oBox.onmousedown = function (ev) {
+            var ev = ev || window.event;
+
+            disX = ev.clientX - oBox.offsetLeft;
+            disY = ev.clientY - oBox.offsetTop;
+
+            document.onmousemove = function (ev) {
+                var ev = ev || window.event;
+
+                var l = ev.clientX - disX;
+                var t = ev.clientY - disY;
+
+                limit(l,t,oBox);
+            }
+
+            document.onmouseup = function () {
+                document.onmousemove = document.onmouseup = null;
+            }
+
+            return false;
+
+        }
     }
-    Person.prototype.showName = function(){
-        alert(this.name);
-    }
-    var a = new Person("张三",20);
-    a.showName();
-})();
+
+    exports.Drag = Drag;
+
+
+});
